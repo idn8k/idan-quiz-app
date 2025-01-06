0a49abb82a8104472884f39d88efa8f6cd721381
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-01-06 16:26:01 +0100

    add toggle answer

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const bookmarkBtn = document.querySelector('[data-js="bookmark-btn"]');
 
 bookmarkBtn.addEventListener('click', () => {
+   toggleBookmarkBtn();
+});
+
+function toggleBookmarkBtn() {
    if (!bookmarkBtn.classList.contains('active')) {
       bookmarkBtn.setAttribute('src', '../assets/card-Bookmark-filled.svg');
       bookmarkBtn.classList.add('active');
@@ -8,4 +12,21 @@ bookmarkBtn.addEventListener('click', () => {
       bookmarkBtn.classList.remove('active');
       bookmarkBtn.setAttribute('src', '../assets/card-Bookmark.svg');
    }
+}
+
+const showAnswerBtn = document.querySelector('[data-js="show-answer"]');
+const answerText = document.querySelector('[data-js="answer-text"]');
+
+showAnswerBtn.addEventListener('click', () => {
+   toggleAnswer();
 });
+
+function toggleAnswer() {
+   if (answerText.classList.contains('hidden')) {
+      answerText.classList.remove('hidden');
+      showAnswerBtn.textContent = 'Hide answer';
+   } else {
+      answerText.classList.add('hidden');
+      showAnswerBtn.textContent = 'Show answer';
+   }
+}
